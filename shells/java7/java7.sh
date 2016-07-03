efbd45132eefa7c9fa1b5fe7b2c77bd27a004839
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-07-03 16:58:11 +0300

    #1 - downloading jdk

--- shells/java7/java7.sh
+++ shells/java7/java7.sh
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+#!bin/bash
+
+echo "Java 7 installing script"
+
+#Downloading java 7 jre
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+
+sudo mkdir -p /usr/lib64/jvm
+
+#tar -xf jdk-7u79-linux-x64.tar.gz -C /usr/lib64/jvm
+
+#rm jdk-7u79-linux-x64.tarsss.gz
