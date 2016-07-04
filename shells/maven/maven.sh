11e4e935d4712457418940b1309413e14434f5be
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-07-04 09:47:55 +0300

    #3 - maven downloading and installing

---
+++ shells/maven/maven.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+echo "Apache maven installing script"
+
+#Downloading apache maven
+
+wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+
+#Installing apache maven
+tar -xf apache-maven-3.3.9-bin.tar.gz -C /opt/
+
+rm apache-maven-3.3.9-bin.tar.gz
+
+#Updating variables.sh
+echo M2_HOME=/opt/apache-maven-3.3.9 >> /etc/profile.d/variables.sh
+echo 'M2=$M2_HOME/bin' >> /etc/profile.d/variables.sh
