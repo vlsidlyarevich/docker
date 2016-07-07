a9a419d341e12259256e240eab63630f5476b212
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-07-07 11:14:43 +0300

    #5 - apache tomcat installation

--- shells/soft/soft.sh
+++ shells/soft/soft.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+mkdir ~/soft
+
 #IDEA installation
 wget https://download.jetbrains.com/idea/ideaIU-2016.1.3.tar.gz
 
-mkdir ~/soft
 tar -xf ideaIU-2016.1.3.tar.gz -C ~/soft/
 rm ideaIU-2016.1.3.tar.gz
 
 chmod +x ~/soft/idea-IU-145.1617.8/bin/idea.sh
 
 #Apache tomcat installation
+wget http://www-eu.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz
+
+tar -xf apache-tomcat-8.0.36.tar.gz -C ~/soft/
+rm apache-tomcat-8.0.36.tar.gz
+
+chown -R $(whoami) /opt/apache-tomcat-8.0.36/
+chmod -R 777 /opt/apache-tomcat-8.0.36/
