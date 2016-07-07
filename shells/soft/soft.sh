d3926df3951276827121b7f73fd41bf5b125e551
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-07-07 11:22:45 +0300

    #5 - updating variables.sh

--- shells/soft/soft.sh
+++ shells/soft/soft.sh
@@ -10,7 +10,7 @@ rm ideaIU-2016.1.3.tar.gz
 
 chmod +x ~/soft/idea-IU-145.1617.8/bin/idea.sh
 
-#Apache tomcat installation
+#Apache tomcat 8 installation
 wget http://www-eu.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz
 
 tar -xf apache-tomcat-8.0.36.tar.gz -C ~/soft/
@@ -18,3 +18,7 @@ rm apache-tomcat-8.0.36.tar.gz
 
 chown -R $(whoami) /opt/apache-tomcat-8.0.36/
 chmod -R 777 /opt/apache-tomcat-8.0.36/
+
+#Updating variables.sh
+echo CATALINA_HOME= ~/Soft/tomcat-8.0.36 >> /etc/profile.d/variables.sh
+echo 'PATH=$CATALINA_HOME/bin:$PATH' >> /etc/profile.d/variables.sh
