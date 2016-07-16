3ce2203a8b48700b72abc5724305bde6bea7f079
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-07-16 14:24:05 +0300

    #6 - script done

--- scripts/other/other.sh
+++ scripts/other/other.sh
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash4
+
+apt-get install -y skype
+apt-get install -y google-chrome-stable
+apt-get install -y firefox
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+apt-get install -y terminator
+apt-get install -y guake
