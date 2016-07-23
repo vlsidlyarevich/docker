78293ea85c4f93d043e6aa30034f339e99d1bef4
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-07-23 13:07:49 +0300

    #8 - update alt fix

--- scripts/java8/java8.sh
+++ scripts/java8/java8.sh
@@ -18,12 +18,12 @@ tar -xf jdk-8u92-linux-x64.tar.gz -C /usr/lib64/jvm
 
 rm jdk-8u92-linux-x64.tar.gz
 
-update-alternatives --remove-all javac
-update-alternatives --remove-all java
-update-alternatives --install /usr/bin/javac javac /usr/lib64/jvm/jdk1.8.0_92/bin/javac 1
-update-alternatives --install /usr/bin/java java /usr/lib64/jvm/jdk1.8.0_92/bin/java 1
-update-alternatives --config javac
-update-alternatives --config java
+update-alternatives --install /usr/bin/javaws javaws /usr/lib64/jvm/jdk1.8.0_92/bin/javaws 1 && \
+update-alternatives --install /usr/bin/javac javac /usr/lib64/jvm/jdk1.8.0_92/bin/javac 1 && \
+update-alternatives --install /usr/bin/java java /usr/lib64/jvm/jdk1.8.0_92/bin/java 1 && \
+update-alternatives --set javac /usr/lib64/jvm/jdk1.8.0_92/bin/javac && \
+update-alternatives --set java /usr/lib64/jvm/jdk1.8.0_92/bin/java && \
+update-alternatives --set javaws /usr/lib64/jvm/jdk1.8.0_92/bin/javaws
 
 #Updating variables.sh
 export JAVA_HOME=/usr/lib64/jvm
