4e44f33ac9a7438e0c2c317cad7a703493f89e29
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-08-07 20:22:08 +0300

    #3 - fixed exporting variables

--- scripts/maven/maven.sh
+++ scripts/maven/maven.sh
@@ -3,17 +3,17 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-printf "*********${RED}Apache maven installation${NC}*********\n"
+printf "*********${RED}Apache maven 3 installation${NC}*********\n"
 
-#Downloading apache maven
-
+#Downloading apache maven-3
 wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 
-#Installing apache maven
+#Installing apache maven-3
 tar -xf apache-maven-3.3.9-bin.tar.gz -C /opt/
 
 rm apache-maven-3.3.9-bin.tar.gz
 
 #Updating variables.sh
-echo M2_HOME=/opt/apache-maven-3.3.9 >> /etc/profile.d/variables.sh
-echo 'M2=$M2_HOME/bin' >> /etc/profile.d/variables.sh
+export M2_HOME=/opt/apache-maven-3.3.9
+export M2=$M2_HOME/bin
+export PATH=$PATH:$M2
